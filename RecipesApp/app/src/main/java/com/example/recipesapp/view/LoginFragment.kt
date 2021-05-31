507bb0c0626bb2b26bffc4a638168b6351cfaa30
Kamil Donda <dondakamil@gmail.com>
2021-05-31 21:42:02 +0200

    Create login screen

--- RecipesApp/app/src/main/java/com/example/recipesapp/view/LoginFragment.kt
+++ RecipesApp/app/src/main/java/com/example/recipesapp/view/LoginFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
 
@@ -14,5 +17,18 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavigation = activity?.findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigation?.visibility = View.GONE
+
+        signIn_button.setOnClickListener { signIn() }
+    }
+
+    private fun signIn() {
+        findNavController().navigate(R.id.action_loginFragment_to_home)
     }
 }
