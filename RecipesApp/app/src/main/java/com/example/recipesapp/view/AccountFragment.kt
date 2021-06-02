7c62ba2d228b6a98529a49c221d509622b74a2a6
Kwolik <kwolikzsp6@gmail.com>
2021-06-02 22:18:49 +0200

    Configure AccountFragment

--- RecipesApp/app/src/main/java/com/example/recipesapp/view/AccountFragment.kt
+++ RecipesApp/app/src/main/java/com/example/recipesapp/view/AccountFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.R
 import com.example.recipesapp.view_model.FirebaseViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_account.*
 
 class AccountFragment : Fragment() {
@@ -33,8 +36,19 @@ class AccountFragment : Fragment() {
         }
     }
 
-    // TODO move to Login fragment after logout
     private fun logout() {
-        firebaseViewModel.auth.signOut()
+        firebaseViewModel.logout(requireActivity()).observe(viewLifecycleOwner, Observer {
+            goToLogout(it)
+        })
+    }
+
+    private fun goToLogout(message: String?) {
+        com.example.recipesapp.model.utils.Snackbar(
+            requireView(),
+            message,
+            getString(R.string.sign_in_successfufly),
+            R.id.action_account_to_loginFragment,
+            Snackbar.LENGTH_SHORT
+        )
     }
 }
