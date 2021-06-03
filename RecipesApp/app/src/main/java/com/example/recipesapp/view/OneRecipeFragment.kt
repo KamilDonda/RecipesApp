e39b25c83b0aa5da649bed6993981f2f7212c0d7
Kamil Donda <dondakamil@gmail.com>
2021-06-03 20:03:08 +0200

    Add temporary listView

--- RecipesApp/app/src/main/java/com/example/recipesapp/view/OneRecipeFragment.kt
+++ RecipesApp/app/src/main/java/com/example/recipesapp/view/OneRecipeFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.recipesapp.R
+import kotlinx.android.synthetic.main.fragment_one_recipe.*
 
 class OneRecipeFragment : Fragment() {
 
@@ -14,5 +16,16 @@ class OneRecipeFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_one_recipe, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ingredientList = arrayListOf("Jeden", "Dwa", "Trzy")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, ingredientList)
+
+        ingredients_listView.adapter = adapter
+        
     }
 }
