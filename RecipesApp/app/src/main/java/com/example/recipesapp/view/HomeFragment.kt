5c9287b83545296836f01982ec918c6c1c642ffa
Kamil Donda <dondakamil@gmail.com>
2021-06-06 10:40:07 +0200

    Correct design of My recipes

--- RecipesApp/app/src/main/java/com/example/recipesapp/view/HomeFragment.kt
+++ RecipesApp/app/src/main/java/com/example/recipesapp/view/HomeFragment.kt
@@ -1,14 +1,12 @@
 package com.example.recipesapp.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.R
 import com.example.recipesapp.adapter.MyRecipesAdapter
@@ -46,7 +44,6 @@ class HomeFragment : Fragment() {
                         myRecipesAdapter.notifyDataSetChanged()
                     })
         })
-
         myRecipesAdapter = MyRecipesAdapter(recipesViewModel.my_recipes, recipesViewModel)
 
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -57,8 +54,6 @@ class HomeFragment : Fragment() {
 
         recyclerView = myRecipes_recyclerView.apply {
             adapter = myRecipesAdapter
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
         }
     }
 }
