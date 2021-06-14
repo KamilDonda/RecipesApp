723aaa5fdbe4f661be95155579cb11028ddccb97
Kamil Donda <dondakamil@gmail.com>
2021-06-14 15:27:46 +0200

    Add selecting time

--- RecipesApp/app/src/main/java/com/example/recipesapp/view/EditRecipeFragment.kt
+++ RecipesApp/app/src/main/java/com/example/recipesapp/view/EditRecipeFragment.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.recipesapp.R
 import com.example.recipesapp.utils.RecipeMenu
+import com.example.recipesapp.utils.TimeConverter
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import kotlinx.android.synthetic.main.fragment_edit_recipe.*
 
 class EditRecipeFragment : Fragment() {
@@ -31,7 +34,7 @@ class EditRecipeFragment : Fragment() {
         }
 
         time_button.setOnClickListener {
-
+            selectTime()
         }
 
         meals_button.setOnClickListener {
@@ -40,7 +43,32 @@ class EditRecipeFragment : Fragment() {
     }
 
     private fun setupMenus() {
-        levelMenu = RecipeMenu(requireContext(), level_button, listOf("Easy", "Medium", "Hard"), level_textView)
+        levelMenu = RecipeMenu(
+            requireContext(),
+            level_button,
+            listOf("Easy", "Medium", "Hard"),
+            level_textView
+        )
         mealsMenu = RecipeMenu(requireContext(), meals_button, (1..10).toList(), meals_textView)
+    }
+
+    private fun selectTime() {
+        val picker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(1)
+                .setMinute(0)
+                .setTitleText(R.string.select_time_desc)
+                .build()
+
+        picker.addOnPositiveButtonClickListener {
+            val h = picker.hour
+            val m = picker.minute
+            val time = TimeConverter().hourAndMinuteToLong(h, m)
+
+            time_textView.text = TimeConverter().longToString(time)
+        }
+
+        picker.show(requireFragmentManager(), null)
     }
 }
